445dd3e91862c3f6355c2417b6045aeff3d26aa5
MARYAM AHMADI <maryam-dk@.com>
2022-01-20 19:15:21 +0100

    js2 week 1

--- javascript/javascript1/week1/script.js
+++ javascript/javascript1/week1/script.js
@@ -1,14 +1,14 @@
 //age-ify task
-let yearOfBirth = 1982;
-let yearFuture = 2025;
+const yearOfBirth = 1982;
+const yearFuture = 2025;
 age = yearFuture - yearOfBirth;
 
 console.log("You will be " + age + " years old in " + yearFuture);
 
 //dog year task
-let dogYearOfBirth = 2000;
-let dogYearFuture = 2030;
-let dogYear = dogYearFuture - dogYearOfBirth;
+const dogYearOfBirth = 2000;
+const dogYearFuture = 2030;
+const dogYear = dogYearFuture - dogYearOfBirth;
 let shouldShowResultInDogYears = false;
 
 if(shouldShowResultInDogYears == false){
@@ -17,23 +17,23 @@ if(shouldShowResultInDogYears == false){
 
 
 //house price task
-let volumeInMeters = 8 * 10 * 10;
-let gadenSizeInM2 = 100 ;
+const volumeInMeters = 8 * 10 * 10;
+const gadenSizeInM2 = 100 ;
 let housePrice = volumeInMeters * 2.5 * 1000 + gadenSizeInM2 * 300;
 console.log ("The house price should be " + housePrice + ". If Peter is paying 2.500.000, then he would be paying too much")
 
 
 
 //startup name task
-let firstWords = ["Easy", "Coroprate", "Awesome", "Super", "Helpful", "New"];
-let secondWords = ["Company", "Succesfull", "Bright", "See", "Doings", "Service"];
+const  firstWords = ["Easy", "Coroprate", "Awesome", "Super", "Helpful", "New"];
+const secondWords = ["Company", "Succesfull", "Bright", "See", "Doings", "Service"];
 
 
 const randomNumber1 = Math.floor(Math.random() * 6);
 const randomNumber2 = Math.floor(Math.random() * 6);
 
-let word1 = firstWords[randomNumber1];
-let word2 = secondWords[randomNumber2];
+const word1 = firstWords[randomNumber1];
+const word2 = secondWords[randomNumber2];
 
 let startupName = word1 + word2; 
 console.log(startupName);
